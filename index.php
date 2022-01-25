74722160854cf2674462afc9c10c84bf6235c65c
sya-hid <syarifhidayatullah2020@gmail.com>
2022-01-25 10:53:20 +0700

    landing page

---
+++ index.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <!-- My CSS -->
+    <link rel="stylesheet" href="style.css">
+    <!-- fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Viga&display=swap" rel="stylesheet">
+
+    
+    <title>Sya_Hid</title>
+  </head>
+  <body>
+
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light">
+<div class="container">
+    <a class="navbar-brand" href="#">Sya_Hid</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav ml-auto">
+        <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-item nav-link" href="#">Pricing</a>
+        <a class="nav-item nav-link" href="#">Features</a>
+        <a class="nav-item nav-link" href="#">About</a>
+        <a class="nav-item btn btn-primary tombol" href="#">Join Us</a>
+      </div>
+    </div>
+  </div>
+</nav>
+<!-- End Navbar -->
+<!-- Jumbotron -->
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">Get Work Done <span> Faster </span> <br>and <span>Better</span>  with Us</h1>
+    <a href="#" class="btn btn-primary tombol">OUR WORK</a>
+  </div>
+</div>
+<!-- end jumbotron -->
+<!-- container -->
+<div class="container">
+  <!-- info panel -->
+  <div class="row justify-content-center">
+    <div class="col-10 info-panel">
+<div class="row">
+<div class="col-lg">
+    <img src="img/employee.png" alt="" class='float-left'>
+    <h4>24 Hours</h4>
+    <p>Lorem ipsum dolor sit amet.</p>
+  </div>
+  <div class="col-lg">
+    <img src="img/hires.png" alt="" class='float-left'>
+    <h4>24 Hours</h4>
+    <p>Lorem ipsum dolor sit amet.</p>
+  </div>
+  <div class="col-lg">
+    <img src="img/security.png" alt="" class='float-left'>
+    <h4>24 Hours</h4>
+    <p>Lorem ipsum dolor sit amet.</p>
+  </div>
+
+</div>
+    </div>
+  </div>
+  <!-- end info panel -->
+
+  <!-- wordking space -->
+  <div class="row workingspace">
+  <div class="col-lg-6">
+    <img src="img/workingspace.png" alt="" class='img-fluid'>
+  </div>
+  <div class="col-lg-5">
+    <h3>YOU <span>WORK</span> LIKE AT <span>HOME</h3>
+    <p>Bekerja dengan suasana hati yang lebih baik dan mempelajari hal baru setiap harinya.</p>
+    <a href="" class='btn btn-primary tombol'>Gallery </a>
+  </div>
+
+
+  </div>
+  <!-- end -->
+<!-- testimoni -->
+<section class='testimonial'>
+  <div class="row justify-content-center">
+    <div class="col-lg-8">
+      <h5>"Bekerja dengan suasana hati yang lebih baik dan mempelajari hal baru setiap harinya."</h5>
+    </div>
+  </div>
+
+  <div class="row justify-content-center">
+    <div class="col-lg-6 justify-content-center d-flex">
+    <figure class="figure">
+  <img src="img/img1.png" class="figure-img img-fluid rounded-circle" alt="">
+
+</figure>
+<figure class="figure">
+  <img src="img/img2.png" class="figure-img img-fluid rounded-circle utama" alt="">
+  <figcaption class="figure-caption"><h5>Sunny Ye</h5><span>designer</span></figcaption>
+</figure>
+<figure class="figure">
+  <img src="img/img3.png" class="figure-img img-fluid rounded-circle" alt="">
+
+</figure>
+    </div>
+  </div>
+</section>
+<!-- en tsestimoni -->
+<!-- footr -->
+<div class="row footer">
+<div class="col text-center">
+  2019 All Rights Reserved by Syarif Hidayatullah
+</div>
+</div>
+<!-- end footr -->
+</div>
+<!-- end container -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
